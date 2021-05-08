0227d025606e782f00656589420e42370476ecd9
Gabriel Leal <gabrielleal96.gl@gmail.com>
2021-05-07 21:52:12 -0400

    Implementando as restrições de movimento

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -2,46 +2,37 @@
 using tabuleiro;
 using xadrez;
 
-namespace xadrez_console
-{
-    class Tela
-    {
-        public static void imprimirTabuleiro(Tabuleiro tab)
-        {
-            for (int i=0; i<tab.linhas; i++)
-            {
+namespace xadrez_console {
+    class Tela {
+
+        public static void imprimirTabuleiro(Tabuleiro tab) {
+
+            for (int i=0; i<tab.linhas; i++) {
                 Console.Write(8 - i + " ");
-                for (int j=0; j<tab.colunas; j++)
-                {
-                    imprimirPeca(tab.peca(i, j));
-                    
+                for (int j=0; j<tab.colunas; j++) {
+                   imprimirPeca(tab.peca(i, j));
                 }
                 Console.WriteLine();
             }
             Console.WriteLine("  a b c d e f g h");
         }
 
-        public static void imprimirTabuleiro(Tabuleiro tab, bool[,] posicoesPossiveis)
-        {
+        public static void imprimirTabuleiro(Tabuleiro tab, bool[,] posicoePossiveis) {
+
             ConsoleColor fundoOriginal = Console.BackgroundColor;
             ConsoleColor fundoAlterado = ConsoleColor.DarkGray;
-            for (int i = 0; i < tab.linhas; i++)
-            {
+
+            for (int i = 0; i < tab.linhas; i++) {
                 Console.Write(8 - i + " ");
-                for (int j = 0; j < tab.colunas; j++)
-                {
-                    if (posicoesPossiveis[i, j])
-                    {
+                for (int j = 0; j < tab.colunas; j++) {
+                    if (posicoePossiveis[i, j]) {
                         Console.BackgroundColor = fundoAlterado;
                     }
-                    else
-                    {
+                    else {
                         Console.BackgroundColor = fundoOriginal;
                     }
-
                     imprimirPeca(tab.peca(i, j));
                     Console.BackgroundColor = fundoOriginal;
-
                 }
                 Console.WriteLine();
             }
@@ -49,28 +40,23 @@ namespace xadrez_console
             Console.BackgroundColor = fundoOriginal;
         }
 
-        public static PosicaoXadrez lerPosicaoXadrez()
-        {
+        public static PosicaoXadrez lerPosicaoXadrez() {
             string s = Console.ReadLine();
             char coluna = s[0];
             int linha = int.Parse(s[1] + "");
             return new PosicaoXadrez(coluna, linha);
         }
 
-        public static void imprimirPeca(Peca peca)
-        {
-            if (peca == null)
-            {
+        public static void imprimirPeca(Peca peca) {
+
+            if (peca == null) {
                 Console.Write("- ");
             }
-            else
-            {
-                if (peca.cor == Cor.Branca)
-                {
+            else {
+                if (peca.cor == Cor.Branca) {
                     Console.Write(peca);
                 }
-                else
-                {
+                else {
                     ConsoleColor aux = Console.ForegroundColor;
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.Write(peca);
@@ -79,7 +65,6 @@ namespace xadrez_console
                 Console.Write(" ");
             }
         }
-
 
     }
 }
